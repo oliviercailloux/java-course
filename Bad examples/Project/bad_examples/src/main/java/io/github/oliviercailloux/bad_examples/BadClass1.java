b150af42bbe5d6bc9cc98d4d454bc5032934182d
Olivier Cailloux <olivier.cailloux@gmail.com>
2017-05-13 12:26:05 +0200

    Some bad examples

---
+++ Bad examples/Project/bad_examples/src/main/java/io/github/oliviercailloux/bad_examples/BadClass1.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.bad_examples;
+
+import java.util.LinkedList;
+
+public class BadClass1 {
+	private LinkedList<String> names;
+
+	public LinkedList<String> getNames() {
+		return names;
+	}
+
+	public void setNames(LinkedList<String> names) {
+		this.names = names;
+	}
+
+}
---
+++ Bad examples/Project/bad_examples/src/main/java/io/github/oliviercailloux/bad_examples/BadClass2.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.bad_examples;
+
+import java.util.List;
+
+public class BadClass2 {
+	private List<String> nameList;
+
+	public List<String> getNames() {
+		return nameList;
+	}
+
+	public void setNames(List<String> nameList) {
+		this.nameList = nameList;
+	}
+
+}
